4a1e39a4f8a6c605a110da9a4ab2bad8ad949664
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-16 11:53:33 +0200

    styling LifeLine borders and thickness of the hr

--- src/components/LifeLine.tsx
+++ src/components/LifeLine.tsx
@@ -1,5 +1,9 @@
 const LifeLine = () => {
-  return <div className="rounded-4xl w-[90vw] h-[20vh] bg-gray-300"></div>;
+  return (
+    <div className="flex flex-col justify-center rounded-4xl w-[90vw] h-[20vh] bg-gray-300 border-1 border-gray-400">
+      <hr className="border-t-3"></hr>
+    </div>
+  );
 };
 
 export default LifeLine;
