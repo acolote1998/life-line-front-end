678ae08cd64ac1fd38d99d42e9a627cf7bcb1319
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-16 14:02:20 +0200

    using blue sphere when day is not defined

--- src/components/LifeLineSphere.tsx
+++ src/components/LifeLineSphere.tsx
@@ -1,5 +1,5 @@
 import type { LifeLineSphereType } from "../types/LifeLineSphereType";
-// import BlueSphere from "../components/spheres/BlueSphere";
+import BlueSphere from "../components/spheres/BlueSphere";
 import RedSphere from "../components/spheres/RedSphere";
 import GoldenSphere from "../components/spheres/GoldenSphere";
 import GreenSphere from "../components/spheres/GreenSphere";
@@ -12,10 +12,14 @@ const LifeLineSphere = ({
   style,
 }: LifeLineSphereType) => {
   const calculateSphere = () => {
-    if (score === 10) return <GoldenSphere width={24} height={24} />;
-    if (score > 5) return <GreenSphere width={24} height={24} />;
-    if (score > 2) return <RedSphere width={24} height={24} />;
-    if (score <= 2) return <DarkSphere width={24} height={24} />;
+    if (score) {
+      if (score === 10) return <GoldenSphere width={24} height={24} />;
+      if (score > 5) return <GreenSphere width={24} height={24} />;
+      if (score > 2) return <RedSphere width={24} height={24} />;
+      if (score <= 2) return <DarkSphere width={24} height={24} />;
+    } else {
+      return <BlueSphere width={32} height={32} />;
+    }
   };
   return (
     <div className={className} style={style} key={id}>
--- src/types/LifeLineSphereType.ts
+++ src/types/LifeLineSphereType.ts
@@ -2,7 +2,7 @@ export type LifeLineSphereType = {
   id?: number;
   description?: string;
   date?: string;
-  score: number;
+  score?: number;
   className: string;
   style: object;
 };
