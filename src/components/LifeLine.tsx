3626c66591fb9b0b1f75b5d10427e00ffde0e48e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-16 11:25:24 +0200

    styling of LifeLine div container

--- src/components/LifeLine.tsx
+++ src/components/LifeLine.tsx
@@ -1,5 +1,5 @@
 const LifeLine = () => {
-  return <div className="bg-amber-200">LifeLine</div>;
+  return <div className="rounded-4xl w-[90vw] h-[20vh] bg-gray-300"></div>;
 };
 
 export default LifeLine;
