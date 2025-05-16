2b7ca3f28be1dc6638a5b94c9de1e0e95ebb4748
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-16 10:06:50 +0200

    adding ButtonsContainer

---
+++ src/components/ButtonsContainer.tsx
@@ -0,0 +1,10 @@
+const ButtonsContainer = () => {
+  return (
+    <>
+      <div className="fixed bottom-0 w-full z-10 bg-amber-200">
+        <button>Hello</button>
+      </div>
+    </>
+  );
+};
+export default ButtonsContainer;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 // import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Header from "../components/Header";
+import ButtonsContainer from "../components/ButtonsContainer";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Header></Header>
       <Outlet />
+      <ButtonsContainer></ButtonsContainer>
       {/* <TanStackRouterDevtools /> */}
     </>
   ),
