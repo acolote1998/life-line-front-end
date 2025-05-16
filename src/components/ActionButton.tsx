fa7158e32229626108d542ef51d4f2320a917c96
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-16 10:53:10 +0200

    ActionButton takes text size from props

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -2,18 +2,21 @@ type ActionButtonProps = {
   description: string;
   lateralPadding: string;
   verticalPadding: string;
+  textSize: string;
 };
 
 const ActionButton = ({
   description,
   lateralPadding,
   verticalPadding,
+  textSize,
 }: ActionButtonProps) => {
-  const styling = `border-1 rounded-lg pt-${verticalPadding} pb-${verticalPadding} pl-${lateralPadding} pr-${lateralPadding}`;
+  const stylingButton = `border-1 rounded-lg pt-${verticalPadding} pb-${verticalPadding} pl-${lateralPadding} pr-${lateralPadding}`;
+  const descriptionSize = `text-${textSize}`;
   return (
     <div>
-      <button className={styling}>
-        <h1 className="text-5xl">{description}</h1>
+      <button className={stylingButton}>
+        <h1 className={descriptionSize}>{description}</h1>
       </button>
     </div>
   );
