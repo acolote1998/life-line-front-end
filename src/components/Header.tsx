825f9a59259dd943dd5fbffa7ecc3693ebf5fa9c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-17 20:49:59 +0200

    adjusting header size to keep the app responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <div className="mt-15 fixed top-0 w-full">
+    <div className="mt-15 md:mt-7 lg:mt-7 fixed top-0 w-full">
       <h1
         style={{ color: "var(--main-texts)" }}
-        className="text-center text-7xl"
+        className="text-center text-7xl md:text-6xl lg:text-5xl"
       >
         LifeLine
       </h1>
