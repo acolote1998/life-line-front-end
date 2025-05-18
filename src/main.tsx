04b1fddf003efaa1ba997d94694294b9d83436c4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-18 11:18:14 +0200

    adding toastify to the project

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./css/global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -27,6 +29,7 @@ if (!rootElement.innerHTML) {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ToastContainer autoClose={2000} />
       </QueryClientProvider>
     </StrictMode>
   );
