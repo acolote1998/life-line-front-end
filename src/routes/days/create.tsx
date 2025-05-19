690c8a92ad19480c71d8f2d711967ae95138e64c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-19 09:25:50 +0200

    added loader to the create day

--- src/routes/days/create.tsx
+++ src/routes/days/create.tsx
@@ -4,13 +4,14 @@ import ActionButton from "../../components/ActionButton";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import useCreateDay from "../../hooks/useCreateDay";
+import Loader from "../../components/Loader";
 
 export const Route = createFileRoute("/days/create")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { mutate, isSuccess, isError } = useCreateDay();
+  const { isPending, mutate, isSuccess, isError } = useCreateDay();
   const [inputTextArea, setInputTextArea] = useState("");
   const [inputScore, setInputScore] = useState("");
   const navigate = useNavigate();
@@ -38,56 +39,59 @@ function RouteComponent() {
   }, [isSuccess, isError]);
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-        }}
-        className=" h-screen flex  flex-col items-center justify-center z-20"
-      >
-        <div className="m-3 flex flex-col text-center rounded-xl h-[50vh] w-[80vw]">
-          <input type="text" className="m-4"></input>
-          <textarea
-            onChange={(e) => {
-              setInputTextArea(e.target.value);
-            }}
-            className="bg-white ml-2 mr-2 rounded-xl text-center h-[40vh]"
-            placeholder="What happened today?"
-          ></textarea>
-          <div className="m-5 flex justify-center">
-            <div className="flex items-center bg-amber-200 p-2 rounded-2xl">
-              <Star width={32} height={32} />
-              <span className="ml-4 flex items-center">
-                <input
-                  onChange={(e) => {
-                    setInputScore(e.target.value);
-                  }}
-                  type="number"
-                  placeholder="X"
-                  className="w-10 h-8 text-center rounded border border-amber-400"
-                  style={{ marginRight: "0.25rem" }}
+      {isPending && <Loader style={{ top: "45.1vh", left: "50vw" }}></Loader>}
+      <>
+        <div
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+          }}
+          className=" h-screen flex  flex-col items-center justify-center z-20"
+        >
+          <div className="m-3 flex flex-col text-center rounded-xl h-[50vh] w-[80vw]">
+            <input type="text" className="m-4"></input>
+            <textarea
+              onChange={(e) => {
+                setInputTextArea(e.target.value);
+              }}
+              className="bg-white ml-2 mr-2 rounded-xl text-center h-[40vh]"
+              placeholder="What happened today?"
+            ></textarea>
+            <div className="m-5 flex justify-center">
+              <div className="flex items-center bg-amber-200 p-2 rounded-2xl">
+                <Star width={32} height={32} />
+                <span className="ml-4 flex items-center">
+                  <input
+                    onChange={(e) => {
+                      setInputScore(e.target.value);
+                    }}
+                    type="number"
+                    placeholder="X"
+                    className="w-10 h-8 text-center rounded border border-amber-400"
+                    style={{ marginRight: "0.25rem" }}
+                  />
+                  / 10
+                </span>
+              </div>
+              <div onClick={handleClick}>
+                <ActionButton
+                  textSize="text-2xl"
+                  buttonStyle="border-1 rounded-2xl pt-2.5 pb-2.5 pl-7 pr-7"
+                  description="💾"
                 />
-                / 10
-              </span>
-            </div>
-            <div onClick={handleClick}>
-              <ActionButton
-                textSize="text-2xl"
-                buttonStyle="border-1 rounded-2xl pt-2.5 pb-2.5 pl-7 pr-7"
-                description="💾"
-              />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          className="text-white text-2xl"
-          onClick={() => {
-            navigate({ to: "/" });
-          }}
-        >
-          X
+          <div
+            className="text-white text-2xl"
+            onClick={() => {
+              navigate({ to: "/" });
+            }}
+          >
+            X
+          </div>
         </div>
-      </div>
+      </>
     </>
   );
 }
