17d6f8700112382a1c80f4149f8df3116adc7221
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-19 09:03:53 +0200

    adding Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="z-50 absolute top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2 ">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
