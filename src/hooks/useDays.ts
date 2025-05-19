eb4c34a0600ae3979f77430a10855f1c74a4b0e4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-19 10:50:53 +0200

    increased the retry attempts to 3 when fetching useDays

--- src/hooks/useDays.ts
+++ src/hooks/useDays.ts
@@ -14,7 +14,7 @@ const useDays = () => {
   const { isPending, isError, data, error } = useQuery<DayType[]>({
     queryKey: ["days"],
     queryFn: fetchDays,
-    retry: 1,
+    retry: 3,
   });
   return { isPending, isError, data, error };
 };
