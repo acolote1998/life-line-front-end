56057cb3dd4c7a078420a4de4bb10ca244e27419
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-19 09:18:50 +0200

    making loader take a style prop to customize placement

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,14 @@
-const Loader = () => {
+import type { CSSProperties } from "react";
+
+type propsLoader = {
+  style: CSSProperties;
+};
+const Loader = ({ style }: propsLoader) => {
   return (
-    <div className="z-50 absolute top-[54.1vh] left-[50vw] -translate-x-1/2 -translate-y-1/2 ">
+    <div
+      style={style}
+      className="z-50 absolute top-[54.1vh] left-[50vw] -translate-x-1/2 -translate-y-1/2 "
+    >
       <div className="loader"></div>
     </div>
   );
