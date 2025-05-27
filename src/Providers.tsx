f8fb496bbd12cb4a22aefdc76e517319ff2b9aca
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-27 16:28:45 +0200

    initialising clerk

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
@@ -11,12 +12,18 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 export const Providers = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ToastContainer autoClose={2500} />
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer autoClose={2500} />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 };
 
